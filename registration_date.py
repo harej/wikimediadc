c80cf8e75ddeed44f230e8202b0ba0d4f40b120f
harej <jamesmhare@gmail.com>
2015-12-10 16:00:24 -0500

    Script for filling in registration dates in the user manifest

---
+++ registration_date.py
@@ -0,0 +1,36 @@
+# User Manifest: 14347585
+# Account Creation Date Field: 109850343
+
+
+import arrow
+import tool_labs_utils
+from pypodio2 import api
+from client_settings import *
+
+def main():
+    sql = tool_labs_utils.WMFReplica()  # usage: sql.query(...)
+    c = api.OAuthClient(client_id,client_secret,username,password)
+    manifest = c.Application.get_items(14347585, limit=500)['items']
+    
+    for item in manifest:
+        extant_fields = {field['field_id']: list(field['values'][0].values())
+                         for field in item['fields']}
+        
+        if 109850343 not in extant_fields:  # account creation field not set
+            username = extant_fields[109823938][0]
+            q = ('select user_registration from user '
+                 'where user_name = "{0}"').format(username)
+            reg_date = sql.query('enwiki', q, None)[0][0].decode('utf-8')
+            reg_date = arrow.get(reg_date, 'YYYYMMDDHHmmss')
+            reg_date = reg_date.format('YYYY-MM-DD HH:mm:ss')
+            
+            attributes = {'fields': ['field_id': 109850343,
+                                     'values': [{'start-utc': reg_date}]]}
+
+
+            id = item['item_id']
+
+            print(c.Item.update(id, attributes))
+
+            
+main()
