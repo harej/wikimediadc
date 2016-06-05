8d525d7ecf6915270f7bdb4615d8d3b4b742da58
harej <jamesmhare@gmail.com>
2016-06-05 12:07:32 -0400

    Trying something

--- username_validation.py
+++ username_validation.py
@@ -32,7 +32,7 @@ def main(event_checkin_app=14347171, user_manifest_app=14347585, checkin_usernam
 
         if wiki_username in user_profiles:
             print("Match identified for " + wiki_username)
-            attributes = {'fields': [{'field_id': associated_user_profile, 'values': [{'value': user_profiles[wiki_username]}]}]}
+            attributes = {'fields': [{'field_id': associated_user_profile, 'values': [user_profiles[wiki_username]]}]}
             print(c.Item.update(item, attributes))
 
 
