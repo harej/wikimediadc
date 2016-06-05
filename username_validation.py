f74659107bff5ce609716a77a1ccc502d4960557
harej <jamesmhare@gmail.com>
2016-06-05 14:20:42 -0400

    If no associated user manifest entry, create one

--- username_validation.py
+++ username_validation.py
@@ -19,8 +19,6 @@ def main(event_checkin_app=14347171, user_manifest_app=14347585, checkin_usernam
         wiki_username = extant_fields[manifest_username_field][0]
         user_profiles[wiki_username] = item_id
 
-    print("Loaded " + str(len(user_profiles)) + " user profiles")
-
     # Now going through and checking for matches against `user_profiles`
     for item in checkins:
         item_id = item['item_id']
@@ -30,7 +28,6 @@ def main(event_checkin_app=14347171, user_manifest_app=14347585, checkin_usernam
         wiki_username = wiki_username.replace('_', ' ')
 
         if wiki_username in user_profiles:
-            print("Match identified for " + wiki_username)
 
             attributes = {'fields':
                                     [
@@ -40,6 +37,34 @@ def main(event_checkin_app=14347171, user_manifest_app=14347585, checkin_usernam
                                     ]
                          }
             print(c.Item.update(item_id, attributes))
+
+        else:
+            q = 'select user_id from user where user_name = "{0}";'.format(wiki_username)
+            if sql.query('enwiki', q, None) != None:
+                # create new User Manifest item in Podio
+                # retrieve User Manifest item ID; add to `user_profiles` to prevent duplicate creation
+                # THEN associate check-in with manifest item
+
+                attributes = {'fields':
+                                        [
+                                            {'field_id': manifest_username_field,
+                                             'values': [{'value': wiki_username}]}
+                                        ]
+                             }
+
+                create_the_thing = c.Item.create(user_manifest_app, attributes)
+                new_id = create_the_thing['item_id']  # hope this works?
+
+                user_profiles[wiki_username] = new_id
+
+                attributes = {'fields':
+                                        [
+                                            {'field_id': associated_user_profile,
+                                             'values': [{'value': user_profiles[wiki_username]}]
+                                            }
+                                        ]
+                             }
+                print(c.Item.update(item_id, attributes))
 
 
 if __name__ == "__main__":
