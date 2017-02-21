fdbc3d45505d3b3f117a951c7775e101d7e8d0c2
harej <jamesmhare@gmail.com>
2017-02-20 18:40:23 -0800

    New method for filtering out already-processed user manifest entries

--- registration_date.py
+++ registration_date.py
@@ -3,37 +3,37 @@ import tool_labs_utils
 from pypodio2 import api
 from client_settings import *
 
-def main(username_field=109823938, user_manifest=14347585, account_creation_date_field=109850343):
+def main(username_field=109823938, user_manifest=14347585, account_creation_date_field=109850343, registration_date_retrieved=None):
     sql = tool_labs_utils.WMFReplica()  # usage: sql.query(...)
     c = api.OAuthClient(client_id,client_secret,username,password)
-    manifest = c.Application.get_items(user_manifest, limit=500)['items']
+    #manifest = c.Application.get_items(user_manifest, limit=500)['items']
+    manifest = c.transport.POST('item', 'app', user_manifest, 'filter', filters={registration_date_retrieved: [None]}, limit=500)['items']
     
     for item in manifest:
         extant_fields = {field['field_id']: list(field['values'][0].values())
                          for field in item['fields']}
         
-        if account_creation_date_field not in extant_fields:  # account creation field not set
-            user = extant_fields[username_field][0]
-            q = ('select user_registration from user '
-                 'where user_name = "{0}"').format(user)
-            try:
-                reg_date = sql.query('enwiki', q, None)[0][0]
-            except:
-                continue
+        user = extant_fields[username_field][0]
+        q = ('select user_registration from user '
+             'where user_name = "{0}"').format(user)
+        try:
+            reg_date = sql.query('enwiki', q, None)[0][0]
+        except:
+            continue
             
-            if reg_date == None:
-                reg_date = '1970-01-01 00:00:00'
-            else:
-                reg_date = reg_date.decode('utf-8')
-                reg_date = arrow.get(reg_date, 'YYYYMMDDHHmmss')
-                reg_date = reg_date.format('YYYY-MM-DD HH:mm:ss')
+        if reg_date == None:
+            reg_date = '1970-01-01 00:00:00'
+        else:
+            reg_date = reg_date.decode('utf-8')
+            reg_date = arrow.get(reg_date, 'YYYYMMDDHHmmss')
+            reg_date = reg_date.format('YYYY-MM-DD HH:mm:ss')
             
-            attributes = {'fields': [{'field_id': account_creation_date_field,
-                                     'values': [{'start_utc': reg_date}]}]}
+        attributes = {'fields': [{'field_id': account_creation_date_field,
+                                  'values': [{'start_utc': reg_date}]}]}
 
-            id = item['item_id']
+        id = item['item_id']
 
-            print(c.Item.update(id, attributes))
+        print(c.Item.update(id, attributes))
 
 
 if __name__ == "__main__":          
--- wikimedia_nyc.py
+++ wikimedia_nyc.py
@@ -3,12 +3,17 @@ import editathon, registration_date, username_validation
 def main():
     UM = 15908656
 
-    username_validation.main(event_checkin_app=15908657, user_manifest_app=UM,
-           checkin_username_field=123109864, manifest_username_field=123109855,
-           associated_user_profile=123109868, associated_event_profile=123109869)
+    username_validation.main(event_checkin_app=15908657,
+                             user_manifest_app=UM,
+                             checkin_username_field=123109864,
+                             manifest_username_field=123109855,
+                             associated_user_profile=123109868,
+                             associated_event_profile=123109869)
 
-    registration_date.main(username_field=123109855, user_manifest=UM,
-                           account_creation_date_field=123109856)
+    registration_date.main(username_field=123109855,
+                           user_manifest=UM,
+                           account_creation_date_field=123109856,
+                           registration_date_retrieved=140624733)
 
 if __name__ == '__main__':
     main()
