19c0bf6de10e17ced03b2750729c2b616e975d29
harej <jamesmhare@gmail.com>
2017-02-20 17:39:17 -0800

    Trying things

--- wikimedia_nyc.py
+++ wikimedia_nyc.py
@@ -1,11 +1,7 @@
 import editathon, registration_date, username_validation
 
-def main():
-    U = username_validation()
-    R = registration_date()
-    E = editathon()
-
-    U.main(event_checkin_app=15908657, user_manifest_app=15908656,
+def main(): 
+    username_validation().main(event_checkin_app=15908657, user_manifest_app=15908656,
            checkin_username_field=123109864, manifest_username_field=123109855,
            associated_user_profile=123109868, associated_event_profile=123109869)
 
